08258f7a43860d8b28d0cea21584faa069134b5f
Adeline <adelinedidier@hotmail.com>
2018-02-22 15:31:25 +0100

    modif pour accès dossiers Develine

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 	<link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
 	<!-- CSS Perso -->
 	<link rel="stylesheet" type="text/css" href="css/DMFstyle.css" media="screen" />
-<script
-  src="http://code.jquery.com/jquery-3.3.1.js"
-  integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-  crossorigin="anonymous"></script>
-  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
+	<script
+	src="http://code.jquery.com/jquery-3.3.1.js"
+	integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+	crossorigin="anonymous"></script>
+	<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
 
 
@@ -33,16 +33,16 @@
 
 
 
-		<!-- HEAD PICTURE -->
+	<!-- HEAD PICTURE -->
 
-		<div class="imageheader">
-			<img class="imagerock c" src="img/headerphoto.png" alt="Jeunes sur un rocher">
-		</div>
-		
-		<!-- PRESENTATION -->
-		
-		<div class="container">
-				<div class="presentation">
+	<div class="imageheader">
+		<img class="imagerock c" src="img/headerphoto.png" alt="Jeunes sur un rocher">
+	</div>
+
+	<!-- PRESENTATION -->
+
+	<div class="container">
+		<div class="presentation">
 
 			<h2>Le Shopping Responsable</h2>
 			<p>
@@ -55,25 +55,25 @@
 				Sur cette page vous pouvez ajouter un produit que vous trouverez en adéquation avec nos critères et que vous avez envie de partager avec la communauté Dress Me Fair
 			</p>
 		</div>
-			
-			
-		</div>
 
-		
-		<!--FORMULAIRE AJOUT DE PRODUIT -->
 
-		
-			<?php include('ajout_view.php') ?>			
+	</div>
 
-						
+
+	<!--FORMULAIRE AJOUT DE PRODUIT -->
+
+
+	<?php include('ajout_view.php') ?>			
+
+
 
 </div> <!--DIV CONTAINER FLUID-->
-	
-	<!-- APPLICATION ET EXTENSION -->
 
-		<?php include('extension_view.php') ?>
+<!-- APPLICATION ET EXTENSION -->
 
-	<!-- DIV Container Fluid -->							
+<?php include('extension_view.php') ?>
+
+<!-- DIV Container Fluid -->							
 <!--  FOOTER -->
 
 <footer>
